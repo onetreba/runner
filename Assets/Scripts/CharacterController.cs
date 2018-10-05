e8f0cc8008965c2766799a3c450f9b1f33d97495
Oleksandr Netreba <onetreba@playtika.com>
2018-10-05 19:21:34 +0300

    Add run and jump

---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour {
+
+    private Rigidbody2D _rigidbody;
+    private int _currentJumpsCount;
+
+
+    public LayerMask GroundLayer;
+    public GameObject GroundPoint;
+    public GameObject FrontPoint;
+    public float JumpPower;
+    public float Speed;
+    public int JumpsAvailable;
+
+	// Use this for initialization
+	void Start () {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        bool isGrounded = Physics2D.OverlapCircle(GroundPoint.transform.position, 0.1f, GroundLayer);
+
+        if (isGrounded && _rigidbody.velocity.y < 0)
+        {
+            _currentJumpsCount = 0;
+        }
+
+        if (Input.GetButtonUp("Jump"))
+        {
+            if(_currentJumpsCount < JumpsAvailable)
+            {
+                _rigidbody.velocity = new Vector2(_rigidbody.velocity.x, JumpPower);
+                //_rigidbody.AddForce(new Vector2(0, JumpPower));
+                _currentJumpsCount++;
+            }
+        }
+
+        _rigidbody.velocity = new Vector2(Speed, _rigidbody.velocity.y);
+    }
+}
